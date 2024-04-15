c6a338feca800cabeb2e80ef8dae623822d143f8
Joe Stephen <joestephenk10@gmail.com>
2024-04-15 12:55:47 +0530

    added differenceBy

--- Array methods/difference.js
+++ Array methods/difference.js
@@ -1,6 +1,7 @@
 import lodash from "lodash";
 
 // Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. The order and references of result values are determined by the first array.
+// Note: Unlike _.pullAll, this method returns a new array.
 
 const array = [2, 3, 0, 12, 4, 6, 8, 10];
 const result = lodash.difference(array, [2, 0, 4, 6, 8, 10]);
---
+++ Array methods/differenceBy.js
@@ -0,0 +1,12 @@
+import lodash from "lodash";
+
+// This method is like _.difference except that it accepts iteratee which is invoked for each element of array and values to generate the criterion by which they're compared. The order and references of result values are determined by the first array. The iteratee is invoked with one argument:
+// (value).
+// Note: Unlike _.pullAllBy, this method returns a new array.
+
+const array = [2, 3, 0, 5, 6, 7, 18, 4];
+let iter_greater = (value) => {
+    return value > 5 ? value : undefined;
+};
+const result = lodash.differenceBy(array, [2, 0, 6, 8, 10], iter_greater);
+console.log(result);
