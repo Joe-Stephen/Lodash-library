80699e765ddcb2d7da5a5b4facf5c70bb3fa7d3b
Joe Stephen <joestephenk10@gmail.com>
2024-04-15 12:28:56 +0530

    added difference

---
+++ Array methods/difference.js
@@ -0,0 +1,7 @@
+import lodash from "lodash";
+
+// Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. The order and references of result values are determined by the first array.
+
+const array = [2, 3, 0, 12, 4, 6, 8, 10];
+const result = lodash.difference(array, [2, 0, 4, 6, 8, 10]);
+console.log(result);
