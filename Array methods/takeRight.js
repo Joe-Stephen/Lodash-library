b44cf3722787260e49cb4b87070d2aded45d1b47
Joe Stephen <joestephenk10@gmail.com>
2024-04-16 09:35:00 +0530

    added takeRight

---
+++ Array methods/takeRight.js
@@ -0,0 +1,7 @@
+import lodash from "lodash";
+
+// Creates a slice of array with n elements taken from the end.
+
+const array = [1, 2, 3, 3, 4, 4, 8, 9, 10];
+const result = lodash.takeRight(array, 3);
+console.log(result);
